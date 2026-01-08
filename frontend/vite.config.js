781ce43e60c04e54da3dd74d92942d47ba6b58b5
Andreas Avgoustis <andreasav@alfaedu.gr>
2026-01-08 16:38:18 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite" // Η νέα μέθοδος για v4
+import tailwindcss from "@tailwindcss/vite" 
 import path from "path"
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Ενεργοποίηση του Tailwind
+    tailwindcss(), 
   ],
   resolve: {
     alias: {
-      // Σωστό alias για Node 24 χωρίς __dirname
+      
       "@": path.resolve(path.dirname(new URL(import.meta.url).pathname).substring(1), "src"),
     },
   },
